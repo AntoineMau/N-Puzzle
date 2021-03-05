5f68a98eda974d8de38389fed669fad2051b707f
AntoineMau <antoinemauffret@gmail.com>
2021-03-05 17:51:35 +0100

    personal generator done | npuzzle with normal search solution done

--- npuzzle.py
+++ npuzzle.py
@@ -1,43 +1,98 @@
 #!/usr/bin/python3
 
-from utils import parse
+from utils import *
+import time
 
 class Npuzzle:
-	def __init__(self, size, content):
+	def __init__(self, size, content, cost, parent, heuri):
 		self.size = size
+		self.cost = cost
+		self.heuri = heuri
+		self.parent = parent
 		self.content = content
 
 	def __str__(self):
 		return 'N-Puzzle\nSize: %d\nContent: %s' % (self.size, self.content)
 
 	def pprint(self):
-		print('N-Puzzle')
-		print('Size: %d' % self.size)
+		print('Cost: %d | Heuri: %d | C + H: %d' % (self.cost, self.heuri, self.cost+self.heuri))
 		for i in range(len(self.content)):
 			print('%s' % self.content[i], end='')
-			print(' ', end='') if i % self.size != self.size - 1 else print('')
-
-class Node:
-	def __init__(self, cost, parent, npuzzle, heuristique):
-		self.cost = cost
-		self.parent = parent
-		self.npuzzle = npuzzle
-		self.heuristique = heuristique
+			print(' ', end='') if i%self.size != self.size-1 else print('')
 
 class Queue:
-	def __init__(self, depart):
-		self.closedList = []
-		self.openList = [depart]
+	def __init__(self, npuzzle_start):
+		self.closed = []
+		self.opened = [npuzzle_start]
+
+def p_matrice(content, size):
+	for i in range(len(content)):
+		print('%s' % content[i], end='')
+		print(' ', end='') if i%size != size-1 else print('')
+	print()
+
+def expand(size, content):
+	index = content.index('0')
+	l = []
+	if int(index/size) != 0:
+		l.append(swap(content.copy(), index, index-size))
+	if int(index/size) != size-1:
+		l.append(swap(content.copy(), index, index+size))
+	if int(index%size) != 0:
+		l.append(swap(content.copy(), index, index-1))
+	if int(index%size) != size-1:
+		l.append(swap(content.copy(), index, index+1))
+	return l
 
 def process(npuzzle):
-	node = Node(0, None, npuzzle, 0)
-	queue = Queue(npuzzle.content)
-	while Queue.openList:
-		pass
-	pass
+	if not is_solvable(npuzzle.content.copy(), npuzzle.size):
+		errno('unsolvable')
+	queue = Queue(npuzzle)
+	finish = [str(i) for i in range(npuzzle.size ** 2)]
+	while queue.opened:
+		e = queue.opened[0]
+		if e.content == finish:
+			break
+		else:
+			queue.opened.pop(0)
+			queue.closed.append(e)
+			for s in expand(e.size, e.content):
+				open_content = [elt.content for elt in queue.opened]
+				close_content = [elt.content for elt in queue.closed]
+				if s not in open_content and s not in close_content:
+					queue.opened.append(Npuzzle(e.size, s, e.cost+1, e, exec_heuri(s, e.size)))
+					queue.opened.sort(key=lambda x: x.cost+x.heuri)
+				else:
+					if s in open_content:
+						closed = False
+						s = queue.opened[open_content.index(s)]
+					else:
+						closed = True
+						s = queue.closed[close_content.index(s)]
+					if s.cost > e.cost + 1:
+						s.cost = e.cost + 1
+						s.parent = e
+						if closed:
+							queue.closed.remove(s)
+							queue.opened.append(s)
+							queue.opened.sort(key=lambda x: x.cost+x.heuri)
+	return e
+
+def print_finish(elem):
+	if elem.parent:
+		print_finish(elem.parent)
+	print()
+	for i in range(len(elem.content)):
+		print('%s' % elem.content[i], end='')
+		print(' ', end='') if i%elem.size != elem.size-1 else print('')
 
 if __name__ == "__main__":
+	start_time = time.time()
+
 	npuzzle = Npuzzle(*parse())
-	npuzzle.pprint()
-	process(npuzzle)
+	e = process(npuzzle)
+	print_finish(e)
+	print('step: %d' % e.cost)
+
+	print("--- %s seconds ---" % (time.time() - start_time))
 	exit(0)
--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ from argparse import ArgumentParser, FileType
 
 def errno(error, detail = None):
 	msg_error = {
-		'bad file': 'Error: bad file for N-Puzzle.'
+		'bad file': 'Error: bad file for N-Puzzle.',
+		'unsolvable': 'Error: N-Puzzle is unsolvable.',
 	}
 	print(msg_error[error])
 	exit(1)
@@ -13,12 +14,36 @@ def parse_option():
 	parser.add_argument('file', type=FileType('r'), help='N-puzzle file')
 	return parser.parse_args()
 
+def swap(content, i1, i2):
+	content[i1], content[i2] = content[i2], content[i1]
+	return content
+
+def exec_heuri(content, size):
+	heuri = 0
+	for i in range(size ** 2):
+		sta = content.index(str(i))
+		objX, objY = int(i/size), int(i%size)
+		staX, staY = int(sta/size), int(sta%size)
+		heuri += abs(objX - staX) + abs(objY - staY)
+	return heuri
+
+def is_solvable(npuzzle, size):
+	permutation = 0
+	index0 = npuzzle.index('0')
+	parity0 = (int(index0/size) + index0%size) % 2
+	for i in range(size**2):
+		if npuzzle[i] != str(i):
+			swap(npuzzle, i, npuzzle.index(str(i)))
+			permutation += 1
+	return permutation%2 == parity0
+
 def parse():
 	args = parse_option()
 	f = args.file.read()
 	f = sub(r'#.*', '', f)
 	first_line = True
 	content = []
+	size = 0
 	for elt in f.split('\n'):
 		if match(r'^[ \t\n]*$', elt):
 			pass
@@ -29,6 +54,6 @@ def parse():
 			content += rsplit(r'[ \t\n]+', elt.strip())
 		else:
 			errno('bad file', elt)
-	if size*size != len(content):
+	if size == 0 or size*size != len(content):
 		errno('bad file', elt)
-	return size, content
+	return size, content, 0, None, exec_heuri(content, size)
